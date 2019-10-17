44b1823c593adfe43c3ab91b8ceb2b73896a2110
Corentin91560 <corentin.delage@hotmail.fr>
2019-10-17 22:19:40 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    FILE* config = fopen("config.txt","r");
+    char* recup=malloc(sizeof(char)*200);
+    if(config != NULL){
+        char* ch=malloc(sizeof(char)*200);
+        while((ch = fgetc(config)) != EOF){
+                recup=ch;
+            //printf("%c", recup);
+        }
+        printf("%c",recup[14]);
+        fclose(config);
+
+    }else{
+        printf("file not open");
+    }
+
+    return 0;
+}
